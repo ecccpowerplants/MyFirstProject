44dd35a51a5e79605464efe3f43c5a35c899fc85
willbh15 <62302826+willbh15@users.noreply.github.com>
2020-03-17 17:34:52 -0400

    MyFirstScript_2020-03-17.py
    Added a new function called concat_func() for concatenating arrays of strings. Fixed indentation.

--- MyFirstProject/MyFirstScript.py
+++ MyFirstProject/MyFirstScript.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 def print_func(n):
 	for i in range(n):
 		print('Hey there cowboy...')
+		
+def concat_func(boi1, boi2):
+	c = []
+	for b1, b2 in zip(boi1.flatten(), boi2.flatten()):
+		c.append(b1 + b2)
+	return np.array(c)
